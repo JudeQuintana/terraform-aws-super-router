0887a4075e4d8dabbe0bf714214b2bc51cf56d87
Jude Quintana <jude.quintana@gmail.com>
2026-08-29 19:13:18 -0600

    update

--- generate_routes.tf
+++ generate_routes.tf
@@ -8,7 +8,7 @@ locals {
 module "this_generate_routes_to_other_vpcs" {
   #source  = "JudeQuintana/generate-routes-to-other-vpcs/aws"
   #version = "1.2.0"
-  source = "git@github.com:JudeQuintana/terraform-aws-generate-routes-to-other-vpcs.git?ref=reachability-provenance"
+  source = "git@github.com:JudeQuintana/terraform-aws-generate-routes-to-other-vpcs.git?ref=compiler-semantic-toolchain"
 
   generate_routes_to_other_vpcs = {
     routing_policy            = var.super_router.routing_policy
